afc7ca2be1cb18a558f4a976f6317269d1dc1147
Juan David Ruiz <nuevo>
2024-01-21 23:56:54 -0500

    proyecto

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "prueba2";
+
+// Create connection
+$conn = new mysqli($servername,$username,$password,$dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+$sql = "INSERT INTO empleados(id,nombre, apellido, email)
+VALUES (5050,'Juan', 'David','Juandavidruiz@hotmail.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+$conn->close();
+?>
